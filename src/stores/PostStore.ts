c2439fe6d9de675172f6d9add807c1bf768882ff
Serhii Chernenko <serhii.chernenko@vaimo.com>
2024-08-12 15:17:32 +0200

    task 10 done

--- src/stores/PostStore.ts
+++ src/stores/PostStore.ts
@@ -1,16 +1,61 @@
 import { defineStore, acceptHMRUpdate } from 'pinia'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
 import type { Post } from '@/types'
 import { useCachedFetch } from '@/composables/useCachedFetch'
 
 export const usePostStore = defineStore('PostStore', () => {
     const posts = ref<Post[]>()
     const loadingList = ref(false)
-    function fetchPostList() {}
+    async function fetchPostList() {
+        const { loading, doFetch } = useCachedFetch({
+            data: posts,
+            fetchStrategy: 'stale-refresh-bg',
+        })
+
+        watch(loading, (loading) => {
+            loadingList.value = loading
+        })
+
+        await doFetch('/api/posts?fields=id,title,previewSnippet')
+    }
 
     const loadingSingle = ref(false)
     const post = ref<Post>()
-    function fetchPostSingle() {}
+
+    const route = useRoute()
+    async function fetchPostSingle() {
+        const { id } = route.params
+
+        if (!id) {
+            return
+        }
+
+        const { loading, doFetch } = useCachedFetch({
+            data: post,
+            fetchStrategy: 'stale-refresh-bg',
+        })
+
+        if (!post.value && posts.value?.length) {
+            loadingList.value = true
+
+            const foundPost = posts.value.find(
+                (p) => p.id === parseInt(id as string),
+            )
+
+            if (!foundPost) {
+                return
+            }
+
+            post.value = foundPost
+        }
+
+        watch(loading, (loading) => {
+            loadingList.value = loading
+        })
+
+        await doFetch(`/api/posts/${id}`)
+    }
 
     return {
         // list of posts
